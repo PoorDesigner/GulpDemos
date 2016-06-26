d4ce5bdc29216dd2589468220830f2193936e8f4
vivekh <vivekh@RF-PC>
2016-06-26 17:24:29 +0530

    Add project files.

---
+++ SassCompiler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SassCompiler.Startup))]
+namespace SassCompiler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ SassCompiler/gulpfile.js
@@ -0,0 +1,12 @@
+﻿/// <binding AfterBuild='SassToCssCompiler' />
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('SassToCssCompiler', function () {
+    gulp.src('Sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./Css/'));
+});
+gulp.task('default', function () {
+    //gulp.watch('sass/**/*.scss', ['SassToCssCompiler']);
+});
